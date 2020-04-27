b0ad9dedf153cec8ca9fb7ab4b4be9c4f991f022
Kyrrah Nork <kyrrahnork@gmail.com>
2020-04-26 17:05:41 -0700

    Adds feedback and slug to create()
    now when a new item is added, the slug is passed to the view and we see the new item, with feedback!

--- sp20/application/models/News_model.php
+++ sp20/application/models/News_model.php
@@ -21,16 +21,23 @@ class News_model extends CI_Model {
     
         public function set_news()
         {
-            $this->load->helper('url');
+                $this->load->helper('url');
 
-            $slug = url_title($this->input->post('title'), 'dash', TRUE);
+                $slug = url_title($this->input->post('title'), 'dash', TRUE);
 
-            $data = array(
-                'title' => $this->input->post('title'),
-                'slug' => $slug,
-                'text' => $this->input->post('text')
-            );
+                $data = array(
+                        'title' => $this->input->post('title'),
+                        'slug' => $slug,
+                        'text' => $this->input->post('text')
+                );
 
-            return $this->db->insert('sp20_news', $data);
+                //return $this->db->insert('sp20_news', $data);
+                
+                if($this->db->insert('sp20_news', $data))
+                {//return slug - send to view page
+                        return $slug;
+                }else{//return false
+                        return false;
+                }
         }
 }
--- sp20/application/views/news/create.php
+++ sp20/application/views/news/create.php
@@ -1,4 +1,9 @@
-<?php //application/views/news/create.php ?>
+<?php
+//application/views/news/create.php
+
+$this->load->view($this->config->item('theme') . 'header');
+
+?>
 
 <h2><?php echo $title; ?></h2>
 
@@ -15,3 +20,8 @@
     <input type="submit" name="submit" value="Create news item" />
 
 </form>
+<?php
+
+$this->load->view($this->config->item('theme') . 'footer');
+
+?>
